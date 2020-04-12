03c34c7aac01803beac3c69e2bd0c6213139b240
Programmer Blackwizard <x10geeky@gmail.com>
2020-04-12 16:52:36 +0700

    reinit

--- ios/Classes/StarPrinterPlugin.m
+++ ios/Classes/StarPrinterPlugin.m
@@ -8,76 +8,35 @@
 #import "star_printer-Swift.h"
 #endif
 
-#import "ModelCapability.h"
-#import "PrinterSetting.h"
-#import "GlobalQueueManager.h"
-#import "Communication.h"
-
-#import <StarIO/SMPort.h>
-
-@interface StarPrinterPlugin ()
-
-
-@property (nonatomic) NSString *portName;
-@property (nonatomic) NSString *portSettings;
-@property (nonatomic) NSString *modelName;
-@property (nonatomic) NSString *macAddress;
-
-@property (nonatomic) PaperSizeIndex paperSizeIndex;
-
-@property (nonatomic) StarIoExtEmulation emulation;
-
-@end
-
 @implementation StarPrinterPlugin
 /*
- + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
- [SwiftStarPrinterPlugin registerWithRegistrar:registrar];
- }
- */
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftStarPrinterPlugin registerWithRegistrar:registrar];
+}
+*/
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-    // 1. Create MethodChannel
-    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"star_printer" binaryMessenger:registrar.messenger];
-    // 2. Plugin registration.
-    StarPrinterPlugin* plugin = [[StarPrinterPlugin alloc] init];
-    [registrar addMethodCallDelegate:plugin channel:channel];
+  // 1. Create MethodChannel
+  FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"star_printer" binaryMessenger:registrar.messenger];
+  // 2. Plugin registration.
+  StarPrinterPlugin* plugin = [[StarPrinterPlugin alloc] init];
+  [registrar addMethodCallDelegate:plugin channel:channel];
 }
 
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-    if ([@"getPlatformVersion" isEqualToString:call.method]) {
-        result([self findPrinter]);
-    } else if ([@"getPrinters" isEqualToString:call.method]) {
-        result([self findPrinter]);
-    } else {
-        // NSString *url = call.arguments[@"url"];
-        result(FlutterMethodNotImplemented);
-    }
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([self findPrinter]);
+  } else if ([@"getPrinters" isEqualToString:call.method]) {
+    result([self findPrinter]);
+  } else {
+    // NSString *url = call.arguments[@"url"];
+    result(FlutterMethodNotImplemented);
+  }
 }
 
-- (NSString *)findPrinter {
-    
-    NSArray *portInfoArray;
-    
-    NSError *error = nil;
-    
-    portInfoArray = [SMPort searchPrinter:@"ALL:" :&error];
-    
-    NSLog(@"Printer %@",portInfoArray);
-    
-    
-    for (PortInfo *portInfo in portInfoArray) {
-        self.portName   = portInfo.portName;
-        self.modelName  = portInfo.modelName;
-        self.macAddress = portInfo.macAddress;
-    }
-    
-    NSLog(@"Name: %@",self.portName);
-    NSLog(@"Model Name: %@",self.modelName);
-    NSLog(@"Mac Address: %@",self.macAddress);
-    
-    return @"Hello World Star Plugin!!!!";
+- (NSString  *)findPrinter {
+  return @"Hello World Star Plugin!!!!";
 }
 @end
 
